045b49c4eb2c0b48c07b7885fac69dca9948dd24
a-difazio <angela_933@hotmail.it>
2024-05-03 15:27:25 +0200

    aggiunta esercitazione 3

---
+++ esercitazione_3/scripts/json.js
@@ -0,0 +1,174 @@
+let httpJson = {
+    "collections": [
+        {
+            "id": 1,
+            "type": "Playlist",
+            "title": "Brani che ti piacciono",
+            "subtitle": "Angela",
+            "date": 2024,
+            "artwork": "https://misc.scdn.co/liked-songs/liked-songs-300.png",
+            "items": []
+        },
+        {
+            "id": 2,
+            "type": "Album",
+            "title": "RELAX",
+            "subtitle": "Calcutta",
+            "date": 2023,
+            "artwork": "https://i.scdn.co/image/ab67616d00001e028acaa7da2fb43a4038d962c6",
+            "items": [
+                {
+                    "title": "2minuti",
+                    "length": 2
+                },
+
+                {
+                    "title": "Loneliness",
+                    "length": 3
+                },
+                {
+                    "title": "Controtempo",
+                    "length": 4
+                },
+
+                {
+                    "title": "SSD",
+                    "length": 3
+                }
+            ]
+        },
+        {
+            "id": 3,
+            "type": "Album",
+            "title": "Evergreen",
+            "subtitle": "Calcutta",
+            "date": 2018,
+            "artwork": "https://i.scdn.co/image/ab67616d0000b2732a7922dc3a775970e2eb96dc",
+            "items": [
+                {
+                    "title": "Paracetamolo",
+                    "length": 2
+                },
+
+                {
+                    "title": "Pesto",
+                    "length": 3
+                }
+            ]
+        }
+    ]
+};
+
+
+let homeJson = {
+    "sections": [
+        {
+            "title": "Creato per Mario Rossi",
+            "collections": [
+                {
+                    "title": "Daily Mix 1",
+                    "subtitle": "Calcutta",
+                    "artwork": "https://i.pinimg.com/1200x/9a/41/98/9a4198abfd84448781ae162c4d8bd1a6.jpg"
+                },
+                {
+                    "title": "Daily Mix 2",
+                    "subtitle": "Drake",
+                    "artwork": "https://dailymix-images.scdn.co/v2/img/ab6761610000e5ebee3123e593174208f9754fab/2/en/default"
+                },
+                {
+                    "title": "Daily Mix 3",
+                    "subtitle": "Beyoncé",
+                    "artwork": "https://dailymix-images.scdn.co/v2/img/ab6761610000e5ebc36dd9eb55fb0db4911f25dd/3/en/default"
+                },
+                {
+                    "title": "Daily Mix 4",
+                    "subtitle": "Taylor Swift",
+                    "artwork": "https://dailymix-images.scdn.co/v2/img/ab6761610000e5eb8278b782cbb5a3963db88ada/4/en/default"
+                },
+                {
+                    "title": "Daily Mix 5",
+                    "subtitle": "Coldplay",
+                    "artwork": "https://dailymix-images.scdn.co/v2/img/ab6761610000e5ebd8b9980db67272cb4d2c3daf/5/en/default"
+                },
+                {
+                    "title": "Daily Mix 6",
+                    "subtitle": "Ariana Grande",
+                    "artwork": "https://dailymix-images.scdn.co/v2/img/ab6761610000e5eba8eef8322e55fc49ab436eea/6/en/default"
+                }
+            ]
+        },
+        {
+            "title": "I tuoi mix preferiti",
+            "collections": [
+                {
+                    "title": "Mega Pop Mix",
+                    "subtitle": "Dua Lipa",
+                    "artwork": "https://i.scdn.co/image/ab67706f0000000291f30907bd017597e6f94001"
+                },
+                {
+                    "title": "Mix Rock",
+                    "subtitle": "Arctic Monkeys",
+                    "artwork": "https://i.scdn.co/image/ab67616d0000b27363dcefae395de2e18cf2b932"
+                },
+                {
+                    "title": "Feel Good Indie",
+                    "subtitle": "Milky Chance",
+                    "artwork": "https://i.scdn.co/image/ab67706f0000000290794016d698b0bc9560186f"
+                },
+                {
+                    "title": "Calcutta Mix",
+                    "subtitle": "Calcutta",
+                    "artwork": "https://i.scdn.co/image/ab6761610000e5ebe6f42fb00b849720936df798"
+                },
+                {
+                    "title": "Indie Chill",
+                    "subtitle": "Bon Iver",
+                    "artwork": "https://image-cdn-ak.spotifycdn.com/image/ab67706c0000da841b2d97a8daa8e46ebc7356cd"
+                },
+                {
+                    "title": "Pop Rock Mix",
+                    "subtitle": "Maroon 5",
+                    "artwork": "https://i.scdn.co/image/ab67616d00001e02a15e27cd081f5b8642abebfe"
+                }
+            ]
+        },
+        {
+            "title": "Ascoltate di recente",
+            "collections": [
+                {
+                    "title": "Mix Rock",
+                    "subtitle": "Arctic Monkeys",
+                    "artwork": "https://i.scdn.co/image/ab67616d0000b27363dcefae395de2e18cf2b932"
+                },
+                {
+                    "title": "Daily Mix 6",
+                    "subtitle": "Ariana Grande",
+                    "artwork": "https://dailymix-images.scdn.co/v2/img/ab6761610000e5eba8eef8322e55fc49ab436eea/6/en/default"
+                },
+                {
+                    "title": "Pop Rock Mix",
+                    "subtitle": "Maroon 5",
+                    "artwork": "https://i.scdn.co/image/ab67616d00001e02a15e27cd081f5b8642abebfe"
+                },
+                {
+                    "title": "Daily Mix 1",
+                    "subtitle": "Calcutta",
+                    "artwork": "https://i.pinimg.com/1200x/9a/41/98/9a4198abfd84448781ae162c4d8bd1a6.jpg"
+                },
+                {
+                    "title": "Calcutta Mix",
+                    "subtitle": "Calcutta",
+                    "artwork": "https://i.scdn.co/image/ab6761610000e5ebe6f42fb00b849720936df798"
+                },
+                {
+                    "title": "Daily Mix 5",
+                    "subtitle": "Coldplay",
+                    "artwork": "https://dailymix-images.scdn.co/v2/img/ab6761610000e5ebd8b9980db67272cb4d2c3daf/5/en/default"
+                }
+            ]
+        }
+    ]
+}
+
+export { httpJson, homeJson };
+
